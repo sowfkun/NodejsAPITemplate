8e61bff35cb4923c22e63d89538162875180dd8d
sowfkun <17521184@gm.uit.edu.vn>
2021-12-11 23:42:08 +0700

    update socket io

--- index.mjs
+++ index.mjs
@@ -1,25 +1,25 @@
 /*
  * import libs
  */
+import cors from 'cors';
+import { config } from 'dotenv';
 import express, { json } from 'express';
 import helmet from 'helmet';
+import http from 'http';
 import morgan from 'morgan';
-import cors from 'cors';
-import rfs from 'rotating-file-stream';
 import path from 'path';
-import { config } from 'dotenv';
-config();
-
-/*
- * run database connection
- */
-import ConnectDatabase from './src/models/db.config.mjs';
-ConnectDatabase();
-
+import rfs from 'rotating-file-stream';
+import { Server } from 'socket.io';
 /*
  * import components
  */
 import rootRoute from './src/routes/router.mjs';
+
+// run dot env config
+config();
+
+// run database connection
+// ConnectDatabase();
 
 /*
  * defined variable
@@ -29,11 +29,21 @@ const isProduction = process.env.NODE_ENVIRONMENT === 'production';
 const __dirname = path.resolve();
 
 const app = express();
-app.enable('trust proxy');
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+/*
+ * defined socket io
+ */
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 /*
  * use middleware
  */
+app.enable('trust proxy');
+
 const accessLogStream = rfs.createStream('access.log', {
   interval: '1d', // rotate daily
   path: path.join(__dirname, 'logs'),
@@ -44,7 +54,7 @@ app.use(
     : morgan('tiny'),
 );
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(json());
 
@@ -52,6 +62,9 @@ app.use(json());
  * defined route
  */
 app.use('/api', rootRoute);
+app.get('/api/test', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.get('/', (req, res) => {
   res.status(404).json({
@@ -68,6 +81,6 @@ app.get('*', (req, res) => {
 /*
  * run server
  */
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
